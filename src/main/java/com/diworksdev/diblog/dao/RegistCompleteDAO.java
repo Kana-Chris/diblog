2ddd3c49721e78e7cb1d05cb93f8d5c9125def71
kana yamato <nyapo797@gmail.com>
2024-08-29 22:36:17 +0900

    DateUtil

--- src/main/java/com/diworksdev/diblog/dao/RegistCompleteDAO.java
+++ src/main/java/com/diworksdev/diblog/dao/RegistCompleteDAO.java
@@ -4,6 +4,7 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 import com.diworksdev.diblog.util.DBConnector;
+import com.diworksdev.diblog.util.DateUtil;
 
 public class RegistCompleteDAO{
 	//gender,authority をString からint型にしておく
@@ -14,6 +15,8 @@ public class RegistCompleteDAO{
 	
 	private DBConnector dbConnector = new DBConnector();
 	private Connection connection = dbConnector.getConnection();
+	
+	DateUtil dateUtil = new DateUtil();
 	
 	public int regist(String family_name,String last_name,
 					  String family_name_kana,String last_name_kana,
@@ -33,7 +36,7 @@ public class RegistCompleteDAO{
 			intAuthority = 1;
 		}
 		String sql ="INSERT INTO user_info (family_name,last_name,family_name_kana,last_name_kana,mail,password,"
-				+ "gender,postal_code,prefecture,address_1,address_2,authority) values(?,?,?,?,?,?,?,?,?,?,?,?)";
+				+ "gender,postal_code,prefecture,address_1,address_2,authority,registared_time) values(?,?,?,?,?,?,?,?,?,?,?,?,?)";
 	
 		try {
 			PreparedStatement preparedStatement = connection.prepareStatement(sql);
@@ -50,6 +53,7 @@ public class RegistCompleteDAO{
 			preparedStatement.setString(10,address_1);
 			preparedStatement.setString(11,address_2);
 			preparedStatement.setInt(12,intAuthority);
+			preparedStatement.setString(13, dateUtil.getDate());
 			
 			result = preparedStatement.executeUpdate();
 			
